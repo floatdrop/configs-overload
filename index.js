7fa5664977aa274560ab229f2525769bce7cf4df
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2014-04-24 07:10:21 +0600

    Resolve custom directory to full path
    This was causing requite to throw an error.
    
    Closes #2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var softRequire = function (entity) {
  * @returns {*} config
  */
 var loadDirectory = function (directory, defaultEnv, environment) {
-    directory = directory || path.join(process.cwd(), 'configs');
+    directory = path.resolve(directory) || path.join(process.cwd(), 'configs');
     defaultEnv = defaultEnv || 'default';
     environment = environment || defaultEnv;
 
